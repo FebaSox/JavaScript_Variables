e5cd1b69bbd7f0ab68db0fd1cb355491c7dc0b38
Seba Fox <seba.n.fox@gmail.com>
2024-06-06 11:19:50 -0500

    javascript excercise

--- app.js
+++ app.js
@@ -4,7 +4,26 @@ console.log(
 );
 
 // Exercise 1
-
+let lastName;
+var age;
 // Exercise 2
-
+const firstName = "Sebastian";
+lastName = "Fox";
+age = 32;
 // Exercise 3
+let language ="JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+// Excercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize ="M"
+// Excersize 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookMark = 450;
+let hasRead = true;
