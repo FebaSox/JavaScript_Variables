2c8ff43534a332b9c1e2b5da18f2a78c8c854fa5
Seba Fox <seba.n.fox@gmail.com>
2024-07-29 11:02:05 -0400

    added a few console log's to my code

--- app.js
+++ app.js
@@ -9,21 +9,28 @@ var age;
 // Exercise 2
 const firstName = "Sebastian";
 lastName = "Fox";
-age = 32;
+age = 21;
 // Exercise 3
 let language ="JavaScript";
 let createdYear = 1995;
 let isCaseSensitive = true;
 // Excercise 4
 let price = 19.99;
+console.log('price');
+/////////////////////////////////////////////
 let isOnSale = false;
 let salePercentage = 15;
+/////////////////////////////////////////////
 let stock = 0;
 let inStock = false;
 let selectedSize ="M"
+/////////////////////////////////////////////
 // Excersize 5
 let title = "Name of the Wind";
 let author = "Patrick Rothfuss";
 let pageCount = 722;
 let bookMark = 450;
 let hasRead = true;
+
+console.log(title);
+console.log(author);
